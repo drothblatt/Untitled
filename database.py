a0615ac24584f3db9e78f148a7f4c9fa6e61080a
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-10-13 23:51:36 -0400

    akjsfhdakljflau

--- database.py
+++ database.py
@@ -3,43 +3,40 @@ import sqlite3
 conn = sqlite3.connect("infos.db")
 c = conn.cursor()
 
-# input: username
-# returns: hash of the user's password (hex string, as it is in the database)
-def getUserPassword(username):
-    q="""
-        SELECT user.password
-        FROM user
-        WHERE people.username=username
-    """
-    result  = c.execute(q)
-    print result
-
 # input: username, hash (hexstring) of user's password
 # returns: true if the username is NOT in the database, and a user is created
 # returns; false if the username has been taken
 def newUser(username, passwordHash):
     q="""
-    SELECT user.username
-    FROM user
+    SELECT users.username
+    FROM users
     """
-    usernames=c.execute(q)
-    if (username in usernames):
-        return False
-    else:
-        q="""
-            insert into user values (username,passwordHash);
-            """
-            #I don't know how to make this work using the parameters given. Help Plox.
+    c.execute(q)
+    usernames = c.fetchall()
+    if len(usernames) == 0:
+        q="INSERT INTO users VALUES ('%s','%s')" % (username, passwordHash)
         c.execute(q)
+        conn.commit()
+        return True
+    else:
+        return False
 
-def authenticate(username, passwordHash)
+# input: username-passwordHash pair
+# output: true if the pair match, false if the pair does not
+def authenticate(uName, passwordHash):
     q="""
-    SELECT user.username, user.passwordHash
-    FROM user
-    WHERE user.username = username and user.passwordHash = passwordHash
-    """
-    result = c.execute(q)
-    if (result= ""):
+    SELECT users.username, users.password
+    FROM users
+    WHERE users.username = "%s" and users.password = "%s"
+    """ % (uName, passwordHash)
+    c.execute(q)
+    result = c.fetchall() # turns the result of execute into a list
+    if len(result) == 0:
         return False
     else:
         return True;
+
+#print newUser("yeech", "12345")
+#print authenticate("yeech", "12345")
+#print authenticate("yeech2", "12345")
+#print authenticate("yeech", "11111")
